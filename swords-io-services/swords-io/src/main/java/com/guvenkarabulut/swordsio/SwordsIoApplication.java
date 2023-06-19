bb70e0c31351f2d260682a862d15867e8a4d9c33
guvenkarabulut <guven.07.568@gmail.com>
2023-06-19 20:42:23 +0300

    first commit

---
+++ swords-io-services/swords-io/src/main/java/com/guvenkarabulut/swordsio/SwordsIoApplication.java
@@ -0,0 +1,13 @@
+package com.guvenkarabulut.swordsio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordsIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwordsIoApplication.class, args);
+	}
+
+}
---
+++ swords-io-services/swords-io/src/test/java/com/guvenkarabulut/swordsio/SwordsIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guvenkarabulut.swordsio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwordsIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
